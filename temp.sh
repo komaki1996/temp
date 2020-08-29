4bfd16677b3e7a71919332e8c827cff9f465f608
komaki1996 <komaki1996@yahoo.co.jp>
2020-08-29 22:47:14 +0900

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-sudo rm /etc/rc.local
-sudo cp /home/pi/temp/rc.local /etc/rc.local
-cat /etc/rc.local
+#sudo rm /etc/rc.local
+#sudo cp /home/pi/temp/rc.local /etc/rc.local
+#cat /etc/rc.local
+
+bash -c "$(curl -sL https://raw.githubusercontent.com/sdetweil/MagicMirror_scripts/master/raspberry.sh)"
+bash -c "$(curl -sL https://raw.githubusercontent.com/sdetweil/MagicMirror_scripts/master/screensaveroff.sh)"
+bash -c "$(curl -sL https://raw.githubusercontent.com/sdetweil/MagicMirror_scripts/master/fixuppm2.sh)"
